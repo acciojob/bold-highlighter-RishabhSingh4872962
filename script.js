8853f5f7d7073886a964be93022e219277b3f8f1
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-27 10:29:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	const strong=document.querySelectorAll("strong"); 
+	strong.forEach((ele)=>{
+		ele.style.color="green"
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
+const strong=document.querySelectorAll("strong"); 
+	strong.forEach((ele)=>{
+		ele.style.color="black"
+	})
     
 }
